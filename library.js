a0f52fb4803ddb13164b344bf79501c2d93107bd
sarahhcodes <78184189+sarahhcodes@users.noreply.github.com>
2024-03-29 11:34:23 -0600

    added library generation; changed form to dialog

--- library.js
+++ library.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 const displayLibrary = document.getElementById('displayLibrary');
+const showForm = document.getElementById('showForm');
+const newBookForm = document.getElementById('newBookForm');
 
 function Book(title,author,pages,read) {
     this.title = title;
@@ -26,10 +28,31 @@ myLibrary.push(new Book('Choke', 'Chuck Palahniuk', 250, true));
 
 document.getElementById('enter').addEventListener('click', function() {
     addBookToLibrary();
+    generateLibrary();
+    newBookForm.close();
 });
 
-myLibrary.forEach((book) => {
+//document.getElementById('remove').addEventListener('click', function(){
+    //currentIndex = document.getElementsByClassName('')
+//});
+
+function generateLibrary() {
+    // clear displayLibrary & regenerate with top row
+    displayLibrary.innerHTML = '<td>Title</td><td>Author</td><td>Page Count</td><td>Read</td><td></td>';
     // loop through myLibrary and display each book in a table
-    // need to initalize table in index.html
-    // each iteration should add a new row
+    myLibrary.forEach((book) => {
+        let newRow = document.createElement('tr');
+        newRow.innerHTML = '<td>' + book.title + '</td><td>' + book.author+ '</td><td>' + book.pages + '</td><td>' + book.read + '</td>' + '<td><button id="remove">remove</button></td>';
+        displayLibrary.appendChild(newRow);
+    });
+};
+
+showForm.addEventListener('click', () => {
+    newBookForm.showModal();
 });
+
+document.getElementById('cancel').addEventListener('click', () => {
+    newBookForm.close();
+});
+
+generateLibrary();
